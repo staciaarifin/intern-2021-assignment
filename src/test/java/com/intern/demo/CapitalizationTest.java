3b71382f90af6b1f53ca2850c40f8469f762345f
randi.alfianto <randi.alfianto@gdn-commerce.com>
2021-03-05 18:26:53 +0700

    add unit test

--- src/test/java/com/intern/demo/CapitalizationTest.java
+++ src/test/java/com/intern/demo/CapitalizationTest.java
@@ -1,10 +1,13 @@
 package com.intern.demo;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CapitalizationTest {
 
     @Test
-    public void convertToMap() {
+    public void convertToMapTest() {
+        //TODO COMPLETE THIS
+        assertNull(Capitalization.convertToMap());
     }
 }
